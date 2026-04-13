7f03682411433677edc7470447ef8f95b23d1d98
Laly-L <laylafvb9@gmail.com>
2026-04-13 20:21:12 -0300

    update: calculadora conforme a meta das equipes

--- scripts.js
+++ scripts.js
@@ -1,51 +1,64 @@
 function calcular(){
-    // JS é uma linguagem não tipada (sem tipo de dados)
-    // Number() é uma função que converte texto em número
-
-    /* recupera valores de itens avulsos */
-
-    // recupera o valor informado no elemento com id qtdeArroz
+    
     let qtdeArroz = Number(document.getElementById("qtdeArroz").value)
-    // recupera o valor informado no elemento com id qtdeFeijao
+   
     let qtdeFeijao = Number(document.getElementById("qtdeFeijao").value)
-    // recupera o valor informado no elemento com id qtdeOleo
+  
     let qtdeOleo = Number(document.getElementById("qtdeOleo").value)
-    // recupera o valor informado no elemento com id qtdeMacarrao
+    
     let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
     // calcula o valor total de pontos dos itens avulsos
     let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
-
-    // calcula o valor total das metas
-    // recupera a cor da equipe
     let cor = document.getElementById("cor").value
-    let metaProva1 // declara a variável
+    let metaKit // declara meta de kit alimentação
+    let metaSuplemento // declaração meta de suplemento
+   
     if (cor == "amarela"){
-        metaProva1 = 54
+        metaKit = 54
+        metaSuplemento = 27
     }
     else if (cor == "cinza"){
-        metaProva1 = 51
+        metaKit = 51
+        metaSuplemento = 26
     }
     else if (cor == "laranja"){
-        metaProva1 = 21
+        metaKit = 21
+        metaSuplemento = 11
     }
     else if (cor == "marron"){
-        metaProva1 = 88
+        metaKit = 88
+        metaSuplemento = 44
     }
     else if (cor == "preta"){
-        metaProva1 = 60
+        metaKit = 60
+        metaSuplemento = 30
     }
-
+   
+    let metaLeite = metaKit // meta do leite é a meta do kit
+    if (metaKit % 2 == 0) {// par
+        metaSuplemento = metaKit / 2
+    }
+    else { // ímpar
+        metaSuplemento = (metaKit / 2) + 1
+    }
+    // calcula a meta da doação de sangue
+    let metaSangue = metaSuplemento
+    // calcula pontuação do kit de alimentação
     let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value)
-    if (kitAlimentacao >= metaProva1) {
-        pontos = pontos + 5000 // já garantimos 5000
-        if (kitAlimentacao > metaProva1){
-            // ganhamos 83.33 a mais de ponto por itens que ultrapassaram 60
-            pontos = pontos + (kitAlimentacao - metaProva1) * (5000/metaProva1)
-        }
+    if (kitAlimentacao >= metaKit) {
+        pontos = pontos + (kitAlimentacao * 10)
     }
-    else {
-        pontos = pontos + kitAlimentacao * (5000 / metaProva1)
+    else  {
+        pontos = pontos + (qtdeLatas * (5000/metaSuplemento))
     }
+    // calcula pontuação do leite
+    let qtdeLeite = Number(document.getElementById("qtdeLeite").value)
+    // fazer os ifs
+
+    // calcula pontuação do sangue
+    let qtdeDoacao = Number(document.getElementById("qtdeDoacao").value)
+    // fazer os ifs
     // vamos exibir o resultado para o usuário
     document.getElementById("resultado").innerText = "Pontuação total: " + pontos + " pontos"
+    document.getElementById("resultado").innerText = "Pontuação total: " + pontos.toFixed(2) + " pontos"
 }
